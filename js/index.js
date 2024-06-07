3b7e7ee93d7342b1224519450e292893962974b8
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-07 11:30:18 -0300

    adjust game over situation

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const marioJump = () => {
   mario.classList.add("jump-mario");
@@ -6,6 +7,36 @@ const marioJump = () => {
     mario.classList.remove("jump-mario");
   }, 500);
 }
+
+const gameOver = (marioPosition, pipePosition) => {
+  pipe.style.animation = 'none';
+  pipe.style.left = `${pipePosition}px`;
+
+  mario.style.animation = 'none';
+  mario.style.bottom = `${marioPosition}px`;
+
+  mario.src = '../imgs/game-over.png';
+  mario.style.width = '80px';
+  mario.style.marginLeft = '50px';
+}
+
+const checkLoop = setInterval(() => {
+  // getting the position to the left of the pipe for checking
+  const pipePosition = pipe.offsetLeft;
+
+  const marioPosition = +window.getComputedStyle(mario)
+    .bottom.replace('px', '');;
+
+  console.log(marioPosition)
+
+
+  // game over when touching the pipe
+  if ((pipePosition > 0 && pipePosition <= 120) && marioPosition < 80) {
+    gameOver(marioPosition, pipePosition);
+    clearInterval(checkLoop);
+  }
+
+}, 10);
 
 document.addEventListener("keyup", marioJump);
 document.addEventListener("touchstart", marioJump);
