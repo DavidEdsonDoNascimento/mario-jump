824cdf47b42ce33bf1560f0d47f4a99249b33bad
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-06-07 11:48:27 -0300

    add animation clouds image

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+const clouds = document.querySelector(".clouds");
 
 const marioJump = () => {
   mario.classList.add("jump-mario");
@@ -15,6 +16,10 @@ const gameOver = (marioPosition, pipePosition) => {
   mario.style.animation = 'none';
   mario.style.bottom = `${marioPosition}px`;
 
+  const cloudsPosition = clouds.offsetLeft;
+  clouds.style.animation = 'none';
+  clouds.style.left = `${cloudsPosition}px`;
+
   mario.src = '../imgs/game-over.png';
   mario.style.width = '80px';
   mario.style.marginLeft = '50px';
@@ -27,15 +32,14 @@ const checkLoop = setInterval(() => {
   const marioPosition = +window.getComputedStyle(mario)
     .bottom.replace('px', '');;
 
-  console.log(marioPosition)
-
-
   // game over when touching the pipe
   if ((pipePosition > 0 && pipePosition <= 120) && marioPosition < 80) {
-    gameOver(marioPosition, pipePosition);
+    gameOver(
+      marioPosition, 
+      pipePosition,
+    );
     clearInterval(checkLoop);
   }
-
 }, 10);
 
 document.addEventListener("keyup", marioJump);
